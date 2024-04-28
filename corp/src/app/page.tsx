79e2f1f2bdd5dddede8623e43436634cdc63ef88
Manuel Alingog <alingman01@gmail.com>
2024-04-27 23:58:45 -0700

    Hero component for Home page

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+import homeImg from '/public/home.jpg';
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-   <div>Home Page</div>
+    <>
+      <Hero imgSrc={homeImg} imgAlt="Car Factory" title="Professional Cloud Hosting"/>
+      <div>
+        <div>Home Page</div>
+      </div>
+    </>
   );
 }
