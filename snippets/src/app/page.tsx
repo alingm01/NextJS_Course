c9ce41c47e3c132d83480b6de023026cec554654
Manuel Alingog <alingman01@gmail.com>
2024-04-29 19:45:44 -0700

    display created snippets on home

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,8 +1,25 @@
+import { db } from '@/db';
 
-export default function Home() {
+export default async function Home() {
+
+  interface snippetType {
+    id: number,
+    title: string,
+    code: string,
+  }
+
+  const snippets = await db.snippet.findMany();
+  const renderedSnippets = snippets.map((snippet: snippetType) => {
+    return (
+      <div key={snippet.id}>
+        {snippet.title}
+      </div>
+    )
+  })
+
   return (
     <div>
-      Home Page
+      {renderedSnippets}
     </div>
   );
 }
