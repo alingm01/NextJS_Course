de296629478a5b85406b2bef65ccc10cc68614ff
Manuel Alingog <alingman01@gmail.com>
2024-04-30 22:59:31 -0700

    structure edit form components with edit page

--- snippets/src/app/snippets/[id]/edit/page.tsx
+++ snippets/src/app/snippets/[id]/edit/page.tsx
@@ -1,15 +1,23 @@
+import { db } from '@/db';
+import notFound from '../not-found';
+import SnippetEditForm from '@/components/snippet-edit-form';
+
 interface EditSnippetType {
   params: {
     id: string
   }
 }
 
-export default function EditSnippet(props: EditSnippetType) {
+export default async function EditSnippet(props: EditSnippetType) {
   const id = parseInt(props.params.id)
+  const snippet = await db.snippet.findFirst({
+    where: { id }
+  })
+
+  if(!snippet) {
+    return notFound()
+  }
+
+  return <div><SnippetEditForm snippet={snippet} /></div>
   
-  return (
-    <div>
-      Edit snippet with id {id}
-    </div>
-  )
 }
