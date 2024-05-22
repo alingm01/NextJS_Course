6bcdbc90e2accfc654836d276126027d950bcfad
Manuel Alingog <alingman01@gmail.com>
2024-05-22 12:39:56 -0700

    create Popover box for Create Topic

--- discuss/src/components/topics/topic-create-form.tsx
+++ discuss/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,31 @@
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@nextui-org/react';
+
+import * as actions from '@/actions';
+
 export default function TopicCreateForm() {
-  return <div>Topic Create Form</div>
+  return (
+    <Popover placement='left'>
+      <PopoverTrigger>
+        <Button color='primary'>Create Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={actions.createTopic}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className='text-lg'>Create Topic</h3>
+            <Input label='Name' labelPlacement='outside' placeholder='Name'/>
+            <Textarea label='Description' labelPlacement='outside' placeholder='Describe your topic' />
+            <Button type='submit'>Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+      <Button>Submit</Button>
+    </Popover>
+  )
 }
