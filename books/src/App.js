53cbcbc73f60bd3c513adc08fac47a9513166352
Manuel Alingog <alingman01@gmail.com>
2024-07-09 23:15:10 -0700

    setup createbook function

--- books/src/App.js
+++ books/src/App.js
@@ -1,3 +1,14 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate"
+
 export default function App() {
-  return <div>App</div>
+  const [books, setBooks] =useState([]);
+
+  function createBook(title) {
+   console.log('need to creat book:', title)
+  }
+
+  return (
+    <BookCreate onCreate = {createBook} />
+  )
 }
--- books/src/components/BookCreate.js
+++ books/src/components/BookCreate.js
@@ -1,5 +1,25 @@
-function BookCreate() {
-  return <div>BookCreate</div>
+import { useState } from "react";
+
+function BookCreate({onCreate}) {
+  const [title, setTitle] = useState('')
+  
+  function handleChange(event) {
+    setTitle(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onCreate(title)
+    setTitle('')
+  }
+  return <div>
+    <form onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input value={title} onChange={handleChange} />
+      <button>Create!</button>
+    </form>
+  </div>
+
 }
 
 export default BookCreate;
