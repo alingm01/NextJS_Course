eed5f9c45479bc3600222af2d25a66295d5dd1d2
Manuel Alingog <alingman01@gmail.com>
2024-07-10 19:24:36 -0700

    added css styingling

--- books/src/components/BookCreate.js
+++ books/src/components/BookCreate.js
@@ -12,13 +12,15 @@ function BookCreate({onCreate}) {
     onCreate(title)
     setTitle('')
   }
-  return <div>
+  return (
+  <div className="book-create">
+    <h3>Add a Book</h3>
     <form onSubmit={handleSubmit}>
       <label>Title</label>
-      <input value={title} onChange={handleChange} />
-      <button>Create!</button>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button">Create!</button>
     </form>
-  </div>
+  </div>)
 
 }
 
