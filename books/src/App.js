75eec2fba00eb84b85be796f0a3e1f189717e68d
Manuel Alingog <alingman01@gmail.com>
2024-07-13 23:31:00 -0700

    implement delete book

--- books/src/App.js
+++ books/src/App.js
@@ -5,7 +5,15 @@ import BookList from "./components/BookList";
 export default function App() {
   const [books, setBooks] =useState([]);
 
-  let id = Math.floor(Math.random() * 9999)
+  let id = Math.floor(Math.random() * 9999);
+
+  function deleteBook(id) {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  }
 
   function createBook(title) {
    const updatedBooks = [
@@ -18,7 +26,7 @@ export default function App() {
 
   return (
     <div className="app">
-      <BookList books={books}/>
+      <BookList books={books} onDelete={deleteBook}/>
       <BookCreate onCreate = {createBook} />
     </div>
   )
